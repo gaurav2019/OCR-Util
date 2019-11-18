e0519adaec282c8bf81b6fe15de85d6c5ff3efe5
Kumar Gaurav <kumar.g@atechnos.com>
2019-11-18 17:30:40 +0530

    first

---
+++ ocrlibrary/src/main/java/com/example/ocrlibrary/Myclass.java
@@ -0,0 +1,12 @@
+package com.example.ocrlibrary;
+
+import android.util.Log;
+
+public class Myclass
+{
+    public  static final String TAG="MyCLass";
+    public static void method(String message)
+    {
+        Log.d(TAG,message);
+    }
+}
